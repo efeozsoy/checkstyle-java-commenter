c70c80b078ba35141125656618043b07b980cbc3
Efe <69420550+efeozsoy@users.noreply.github.com>
2022-12-27 13:36:59 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,18 @@ set -e
 
 if [ -n "${GITHUB_WORKSPACE}" ] ; then
   cd "${GITHUB_WORKSPACE}/${INPUT_WORKDIR}" || exit
-  git config --global --add safe.directory "${GITHUB_WORKSPACE}" || exit 1
 fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
+if [ -n "${INPUT_PROPERTIES_FILE}" ]; then
+  OPT_PROPERTIES_FILE="-p ${INPUT_PROPERTIES_FILE}"
+fi
+
 wget -O - -q "https://github.com/checkstyle/checkstyle/releases/download/checkstyle-${INPUT_CHECKSTYLE_VERSION}/checkstyle-${INPUT_CHECKSTYLE_VERSION}-all.jar" > /checkstyle.jar
 
-misspell -locale="${INPUT_LOCALE}" . \
-  | reviewdog -f=checkstyle \
+exec java -jar /checkstyle.jar "${INPUT_WORKDIR}" -c "${INPUT_CHECKSTYLE_CONFIG}" ${OPT_PROPERTIES_FILE} -f xml \
+ | reviewdog -f=checkstyle \
       -name="checkstyle" \
       -reporter="${INPUT_REPORTER:-github-pr-check}" \
       -filter-mode="${INPUT_FILTER_MODE}" \
